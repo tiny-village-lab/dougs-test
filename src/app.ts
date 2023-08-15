179080ac8d87030f4955a3455f87777749a1a056
benoit j <bjupille@gmail.com>
2023-08-15 10:04:03 +0200

    Added first route for the movements

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import { router as movementRoutes } from "./routes/movement.routes";
 
 const app: Application = express();
 
+app.use("/movements", movementRoutes);
+
 app.use("/", (req: Request, res: Response, next: NextFunction): void => {
-  res.json({ message: "Allo! Catch-all route." });
+    res.json({ message: "Allo! Catch-all route." });
 });
 
 export default app;
---
+++ src/routes/movement.routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.post("/validation", (req: Request, res: Response): void => {
+    
+});
+
+export { router };
